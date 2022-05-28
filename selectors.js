ad898d97a06a10492294b7d9d30e77b2b9a57d15
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2022-05-28 13:12:51 +0200

    Update selectors.js

--- selectors.js
+++ selectors.js
@@ -2,11 +2,14 @@
 https://github.com/Pulgasari/selectors.js
 ****************************************/
 
+// Select Single Elements
 const $       = (selector) => document.querySelector(selector);
 const $CLASS  = (selector) => document.getElementsByClassName(selector)[0];
 const $ID     = (selector) => document.getElementById(selector);
 const $NAME   = (selector) => document.getElementsByName(selector)[0];
 const $TAG    = (selector) => document.getElementsByTagName(selector)[0];
+
+// Select Multiple Elements as Array
 const $$      = (selector) => document.querySelectorAll(selector);
 const $$CLASS = (selector) => document.getElementsByClassName(selector);
 const $$NAME  = (selector) => document.getElementsByName(selector);
