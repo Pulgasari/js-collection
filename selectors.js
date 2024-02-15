aa28c93f89ca57c5fb970ff43bd3ef8502c5dec2
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2024-02-15 03:11:42 +0100

    Update selectors.js

--- selectors.js
+++ selectors.js
@@ -1,16 +1,17 @@
-/****************************************
-https://github.com/Pulgasari/selectors.js
-****************************************/
+//  $ = select single element
+// $$ = select multiple elements
 
-// Select Single Elements
-const $       = (selector) => document.querySelector(selector);
-const $CLASS  = (selector) => document.getElementsByClassName(selector)[0];
-const $ID     = (selector) => document.getElementById(selector);
-const $NAME   = (selector) => document.getElementsByName(selector)[0];
-const $TAG    = (selector) => document.getElementsByTagName(selector)[0];
+const       $ = ( selector, context ) => ( context || document ).querySelector(selector);
+const      $$ = ( selector, context ) => ( context || document ).querySelectorAll(selector);
+const  $CLASS = ( selector, context ) => ( context || document ).getElementsByClassName(selector)[0];
+const $$CLASS = ( selector, context ) => ( context || document ).getElementsByClassName(selector);
+const     $ID = ( selector, context ) => ( context || document ).getElementById(selector);
+const   $NAME = ( selector, context ) => ( context || document ).getElementsByName(selector)[0];
+const  $$NAME = ( selector, context ) => ( context || document ).getElementsByName(selector);
+const    $TAG = ( selector, context ) => ( context || document ).getElementsByTagName(selector)[0];
+const   $$TAG = ( selector, context ) => ( context || document ).getElementsByTagName(selector);
 
-// Select Multiple Elements as Array
-const $$      = (selector) => document.querySelectorAll(selector);
-const $$CLASS = (selector) => document.getElementsByClassName(selector);
-const $$NAME  = (selector) => document.getElementsByName(selector);
-const $$TAG   = (selector) => document.getElementsByTagName(selector);
+const   $ATTR = ( selector, value ) =>  $('['      + selector + ( value ? '="' + value + '"]' : ']' ) )[0];
+const  $$ATTR = ( selector, value ) => $$('['      + selector + ( value ? '="' + value + '"]' : ']' ) );
+const   $DATA = ( selector, value ) =>  $('[data-' + selector + ( value ? '="' + value + '"]' : ']' ) )[0];
+const  $$DATA = ( selector, value ) => $$('[data-' + selector + ( value ? '="' + value + '"]' : ']' ) );
