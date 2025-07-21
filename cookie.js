18f81dc8ca12bbea340f4c7a5c8182d376380cca
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2025-07-21 22:00:12 +0200

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,26 +1,29 @@
 (function(){ "use strict";
-
   this.cookie = ( name, value, days, domain ) => {
-  
+    // ?
+    if( name.startsWith('?') ){
+      let key = name.slice(1);
+      let result = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+      // has cookie / does cookie exist
+      if( value === undefined ) return !!result;
+      // is cookie (value of)
+      return result ? result[2] === value : false;
+    }
+    // get cookie
     if( !name.startsWith('!') && !value && !days ){
-  
-      // get cookie
       let result = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
       return result ? result[2] : null;
-  
-    } else {
-  
-      // set values for removing cookie
-      name.startsWith('!') ? ( value = '', days = '-1' ) : '' ;
-  
-      // set cookie
-      let date = new Date;
-      date.setTime( date.getTime() + 24 * 60 * 60 * 1000 * ( days || 1000) );
-      let theDomain = domain ? ';domain=' + domain : '';
-      document.cookie = name + '=' + value + ';path=/;expires=' + date.toGMTString() + theDomain;
-  
     }
-  
+    // (set values to) delete cookie
+    if( name.startsWith('!') ){
+      name = name.slice(1)
+      value = ''
+      days = -1
+    }
+    // set cookie
+    let date = new Date;
+    date.setTime( date.getTime() + 24 * 60 * 60 * 1000 * ( days || 1000 ) );
+    let theDomain = domain ? ';domain=' + domain : '';
+    document.cookie = name + '=' + value + ';path=/;expires=' + date.toGMTString() + theDomain;
   }
-
-}).call(this);
+}).call(this)
