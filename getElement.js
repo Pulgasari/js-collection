f213cfd024b4cff26d2cf046e382e53abfa6d7b6
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2025-08-08 00:52:05 +0200

    Create getElement.js

---
+++ getElement.js
@@ -0,0 +1,3 @@
+const getElement     = selector => document.querySelector(selector);
+const getElements    = selector => document.querySelectorAll(selector);
+const getElementById = id       => document.getElementById(id);
