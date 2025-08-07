c0d84a3fb36c7dfad262a2073f2b996083104678
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2025-08-08 01:15:11 +0200

    Update getElement.js

--- getElement.js
+++ getElement.js
@@ -1,3 +1,15 @@
-const getElement     = selector => document.querySelector(selector);
-const getElements    = selector => document.querySelectorAll(selector);
-const getElementById = id       => document.getElementById(id);
+const getElement      = selector => document.querySelector(selector);
+const getElements     = selector => document.querySelectorAll(selector);
+// Single
+const getElementById        = id => document.getElementById(id);
+// Multiple
+const getElementsByClass  = name => document.getElementsByClassName(name);
+const getElementsByName   = name => document.getElementsByName(name);
+const getElementsByTag    = name => document.getElementsByTagName(name);
+// Special: by DataKey
+const  getElementByDataKey = key => document.getElement ('[data-key="' + key + '"]');
+const getElementsByDataKey = key => document.getElements('[data-key="' + key + '"]');
+// Special Shorthand Constants
+let $root = document.documentElement;
+let $body = document.body;
+let $head = document.head;
