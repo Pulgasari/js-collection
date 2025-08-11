60adf45de1d7110dc4f5dab50dd3ecd75ff019a2
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2025-08-11 14:55:50 +0200

    Update getElement.js

--- getElement.js
+++ getElement.js
@@ -3,9 +3,9 @@ const getElements     = selector => document.querySelectorAll(selector);
 // Single
 const getElementById        = id => document.getElementById(id);
 // Multiple
-const getElementsByClass  = name => document.getElementsByClassName(name);
-const getElementsByName   = name => document.getElementsByName(name);
-const getElementsByTag    = name => document.getElementsByTagName(name);
+const getElementsByClass  = name => Array.from( document.getElementsByClassName(name) );
+const getElementsByName   = name => Array.from( document.getElementsByName(name)      );
+const getElementsByTag    = name => Array.from( document.getElementsByTagName(name)   );
 // Special: by DataKey
 const  getElementByDataKey = key => document.getElement ('[data-key="' + key + '"]');
 const getElementsByDataKey = key => document.getElements('[data-key="' + key + '"]');
